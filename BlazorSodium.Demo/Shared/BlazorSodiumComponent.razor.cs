65f44be55aba5bf53f7e42a1c605fcf922ce8950
Jack-Edwards <jackedwards@protonmail.com>
2022-10-07 15:05:40 -0500

    wip

--- BlazorSodium.Demo/Shared/BlazorSodiumComponent.razor.cs
+++ BlazorSodium.Demo/Shared/BlazorSodiumComponent.razor.cs
@@ -8,26 +8,22 @@ using System.Runtime.InteropServices.JavaScript;
 
 namespace BlazorSodium.Demo.Shared
 {
-   public partial class BlazorSodiumComponent
+public partial class BlazorSodiumComponent
+{
+   protected override async Task OnInitializedAsync()
    {
-      protected override async Task OnInitializedAsync()
-      {
-         await JSHost.ImportAsync("blazorSodium", "../_content/BlazorSodium/blazorSodium.bundle.js");
-         //Interop.Test();
-         await Interop.InitializeAsync();
-      }
-
-      public partial class Interop
-      {
-         static Interop()
-         {
-         }
-
-         [JSImport("sodium.ready", "blazorSodium")]
-         public static partial Task InitializeAsync();
-
-         [JSImport("test", "blazorSodium")]
-         public static partial void Test();
-      }
+      await JSHost.ImportAsync("blazorSodium", "../_content/BlazorSodium/blazorSodium.bundle.js");
+      await Interop.InitializeAsync();
    }
+
+   public partial class Interop
+   {
+      static Interop()
+      {
+      }
+
+      [JSImport("sodium.ready", "blazorSodium")]
+      public static partial Task InitializeAsync();
+   }
+}
 }
