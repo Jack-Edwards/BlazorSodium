7652ee1199d94c0386b40c1b9e02826158a39b9e
Jack-Edwards <jackedwards@protonmail.com>
2022-10-11 21:43:31 -0500

    Implement and demonstrate Ed25519 key pair generation

---
+++ BlazorSodium/Sodium/PublicKeySignature.cs
@@ -0,0 +1,30 @@
+﻿using BlazorSodium.Sodium.Models;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace BlazorSodium.Sodium
+{
+   public static partial class PublicKeySignature
+   {
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <returns></returns>
+      /// <see cref="https://github.com/jedisct1/libsodium.js/blob/master/wrapper/symbols/crypto_sign_keypair.json"/>
+      [JSImport("sodium.crypto_sign_keypair", "blazorSodium")]
+      internal static partial JSObject Crypto_Sign_KeyPair_Internal();
+
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <returns></returns>
+      /// <see cref="https://github.com/jedisct1/libsodium.js/blob/master/wrapper/symbols/crypto_sign_keypair.json"/>
+      public static KeyPair Crypto_Sign_KeyPair()
+      {
+         JSObject jsObject = Crypto_Sign_KeyPair_Internal();
+         string keyType = jsObject.GetPropertyAsString("keyType");
+         byte[] privateKey = jsObject.GetPropertyAsByteArray("privateKey");
+         byte[] publicKey = jsObject.GetPropertyAsByteArray("publicKey");
+         return new KeyPair(keyType, privateKey, publicKey);
+      }
+   }
+}
