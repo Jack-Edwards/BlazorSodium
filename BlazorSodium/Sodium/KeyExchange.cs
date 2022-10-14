dc297b64eae6bffdbc4366688b43507256150042
Jack-Edwards <jackedwards@protonmail.com>
2022-10-14 18:27:39 -0500

    Implement 'KeyPair' generation methods and models

---
+++ BlazorSodium/Sodium/KeyExchange.cs
@@ -0,0 +1,51 @@
+﻿using BlazorSodium.Sodium.Models;
+using System.Runtime.InteropServices.JavaScript;
+using System.Runtime.Versioning;
+
+namespace BlazorSodium.Sodium
+{
+   public static partial class KeyExchange
+   {
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <returns></returns>
+      /// <see cref="https://github.com/jedisct1/libsodium.js/blob/master/wrapper/symbols/crypto_kx_keypair.json"/>
+      [SupportedOSPlatform("browser")]
+      public static X25519KeyPair Crypto_KX_KeyPair()
+      {
+         JSObject jsObject = Crypto_KX_KeyPair_Internal();
+         return X25519KeyPair.FromJavaScript(jsObject);
+      }
+
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <returns></returns>
+      /// <see cref="https://github.com/jedisct1/libsodium.js/blob/master/wrapper/symbols/crypto_kx_keypair.json"/>
+      [JSImport("sodium.crypto_kx_keypair", "blazorSodium")]
+      internal static partial JSObject Crypto_KX_KeyPair_Internal();
+
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <param name="seed"></param>
+      /// <returns></returns>
+      /// <see cref="https://github.com/jedisct1/libsodium.js/blob/master/wrapper/symbols/crypto_kx_seed_keypair.json"/>
+      [SupportedOSPlatform("browser")]
+      public static X25519KeyPair Crypto_KX_Seed_KeyPair(byte[] seed)
+      {
+         JSObject jsObject = Crypto_KX_Seed_KeyPair_Internal(seed);
+         return X25519KeyPair.FromJavaScript(jsObject);
+      }
+
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <param name="seed"></param>
+      /// <returns></returns>
+      /// <see cref="https://github.com/jedisct1/libsodium.js/blob/master/wrapper/symbols/crypto_kx_seed_keypair.json"/>
+      [JSImport("sodium.crypto_kx_seed_keypair", "blazorSodium")]
+      internal static partial JSObject Crypto_KX_Seed_KeyPair_Internal(byte[] seed);
+   }
+}
--- BlazorSodium/Sodium/PublicKeySignature.cs
+++ BlazorSodium/Sodium/PublicKeySignature.cs
@@ -1,10 +1,23 @@
 ﻿using BlazorSodium.Sodium.Models;
 using System.Runtime.InteropServices.JavaScript;
+using System.Runtime.Versioning;
 
 namespace BlazorSodium.Sodium
 {
    public static partial class PublicKeySignature
    {
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <returns></returns>
+      /// <see cref="https://github.com/jedisct1/libsodium.js/blob/master/wrapper/symbols/crypto_sign_keypair.json"/>
+      [SupportedOSPlatform("browser")]
+      public static Ed25519KeyPair Crypto_Sign_KeyPair()
+      {
+         JSObject jsObject = Crypto_Sign_KeyPair_Internal();
+         return Ed25519KeyPair.FromJavaScript(jsObject);
+      }
+
       /// <summary>
       /// 
       /// </summary>
@@ -16,15 +29,23 @@ namespace BlazorSodium.Sodium
       /// <summary>
       /// 
       /// </summary>
+      /// <param name="seed"></param>
       /// <returns></returns>
-      /// <see cref="https://github.com/jedisct1/libsodium.js/blob/master/wrapper/symbols/crypto_sign_keypair.json"/>
-      public static KeyPair Crypto_Sign_KeyPair()
+      /// <see cref="https://github.com/jedisct1/libsodium.js/blob/master/wrapper/symbols/crypto_sign_seed_keypair.json"/>
+      [SupportedOSPlatform("browser")]
+      public static Ed25519KeyPair Crypto_Sign_Seed_KeyPair(byte[] seed)
       {
-         JSObject jsObject = Crypto_Sign_KeyPair_Internal();
-         string keyType = jsObject.GetPropertyAsString("keyType");
-         byte[] privateKey = jsObject.GetPropertyAsByteArray("privateKey");
-         byte[] publicKey = jsObject.GetPropertyAsByteArray("publicKey");
-         return new KeyPair(keyType, privateKey, publicKey);
+         JSObject jsObject = Crypto_Sign_Seed_KeyPair_Internal(seed);
+         return Ed25519KeyPair.FromJavaScript(jsObject);
       }
+
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <param name="seed"></param>
+      /// <returns></returns>
+      /// <see cref="https://github.com/jedisct1/libsodium.js/blob/master/wrapper/symbols/crypto_sign_seed_keypair.json"/>
+      [JSImport("sodium.crypto_sign_seed_keypair", "blazorSodium")]
+      internal static partial JSObject Crypto_Sign_Seed_KeyPair_Internal(byte[] seed);
    }
 }
