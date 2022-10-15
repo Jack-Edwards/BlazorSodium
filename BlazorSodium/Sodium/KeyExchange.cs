d368c303bfe9bd1e99c41be0dd0f8d44cecd8531
Jack-Edwards <jackedwards@protonmail.com>
2022-10-15 14:08:53 -0500

    Add remaining key exchange methods

--- BlazorSodium/Sodium/KeyExchange.cs
+++ BlazorSodium/Sodium/KeyExchange.cs
@@ -9,6 +9,56 @@ namespace BlazorSodium.Sodium
       /// <summary>
       /// 
       /// </summary>
+      /// <param name="clientKeyPair"></param>
+      /// <param name="serverPublicKey"></param>
+      /// <returns></returns>
+      /// <see cref="https://github.com/jedisct1/libsodium.js/blob/master/wrapper/symbols/crypto_kx_client_session_keys.json"/>
+      [SupportedOSPlatform("browser")]
+      public static SessionKeys Crypto_KX_Client_Session_Keys(X25519KeyPair clientKeyPair, byte[] serverPublicKey)
+      {
+         JSObject jsObject = Crypto_KX_Client_Session_Keys_Internal(clientKeyPair.PublicKey, clientKeyPair.PrivateKey, serverPublicKey);
+         return SessionKeys.FromJavaScript(SessionKeysType.Client, jsObject);
+      }
+
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <param name="clientPublicKey"></param>
+      /// <param name="clientSecretKey"></param>
+      /// <param name="serverPublicKey"></param>
+      /// <returns></returns>
+      /// <see cref="https://github.com/jedisct1/libsodium.js/blob/master/wrapper/symbols/crypto_kx_client_session_keys.json"/>
+      [JSImport("sodium.crypto_kx_client_session_keys", "blazorSodium")]
+      internal static partial JSObject Crypto_KX_Client_Session_Keys_Internal(byte[] clientPublicKey, byte[] clientSecretKey, byte[] serverPublicKey);
+
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <param name="serverKeyPair"></param>
+      /// <param name="clientPublicKey"></param>
+      /// <returns></returns>
+      /// <see cref="https://github.com/jedisct1/libsodium.js/blob/master/wrapper/symbols/crypto_kx_server_session_keys.json"/>
+      [SupportedOSPlatform("browser")]
+      public static SessionKeys Crypto_KX_Server_Session_Keys(X25519KeyPair serverKeyPair, byte[] clientPublicKey)
+      {
+         JSObject jsObject = Crypto_KX_Server_Session_Keys_Internal(serverKeyPair.PublicKey, serverKeyPair.PrivateKey, clientPublicKey);
+         return SessionKeys.FromJavaScript(SessionKeysType.Server, jsObject);
+      }
+
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <param name="serverPublicKey"></param>
+      /// <param name="serverSecretKey"></param>
+      /// <param name="clientPublicKey"></param>
+      /// <returns></returns>
+      /// <see cref="https://github.com/jedisct1/libsodium.js/blob/master/wrapper/symbols/crypto_kx_server_session_keys.json"/>
+      [JSImport("sodium.crypto_kx_server_session_keys", "blazorSodium")]
+      internal static partial JSObject Crypto_KX_Server_Session_Keys_Internal(byte[] serverPublicKey, byte[] serverSecretKey, byte[] clientPublicKey);
+
+      /// <summary>
+      /// 
+      /// </summary>
       /// <returns></returns>
       /// <see cref="https://github.com/jedisct1/libsodium.js/blob/master/wrapper/symbols/crypto_kx_keypair.json"/>
       [SupportedOSPlatform("browser")]
---
+++ BlazorSodium/Sodium/Models/SessionKeys.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices.JavaScript;
+using System.Runtime.Versioning;
+
+namespace BlazorSodium.Sodium.Models
+{
+   public enum SessionKeysType
+   {
+      Client,
+      Server
+   }
+
+   public class SessionKeys
+   {
+      public SessionKeysType KeysType { get; init; }
+      public byte[] ReceiveKey { get; init; }
+      public byte[] TransmitKey { get; init; }
+
+      public SessionKeys(SessionKeysType keysType, byte[] receiveKey, byte[] transmitKey)
+      {
+         KeysType = keysType;
+         ReceiveKey = receiveKey;
+         TransmitKey = transmitKey;
+      }
+
+      [SupportedOSPlatform("browser")]
+      public static SessionKeys FromJavaScript(SessionKeysType keysType, JSObject jsObject)
+      {
+         byte[] receiveKey = jsObject.GetPropertyAsByteArray("sharedRx");
+         byte[] transmitKey = jsObject.GetPropertyAsByteArray("sharedTx");
+         return new SessionKeys(keysType, receiveKey, transmitKey);
+      }
+   }
+}
