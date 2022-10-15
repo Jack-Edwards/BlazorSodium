bed9a1910ff7b96d9482c777b5baf99bbba97e56
Jack-Edwards <jackedwards@protonmail.com>
2022-10-15 14:07:53 -0500

    Minify and compress the JavaScript bundle

--- BlazorSodium/webpack.config.js
+++ BlazorSodium/webpack.config.js
@@ -1,10 +1,27 @@
 ﻿const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
    name: 'blazorSodium',
    target: 'web',
+   mode: 'production',
    experiments: {
       outputModule: true,
+   },
+   optimization: {
+      minimize: true,
+      minimizer: [
+         new TerserPlugin({
+            terserOptions: {
+               compress: {
+                  drop_console: true,
+                  passes: 3
+               },
+               mangle: true
+            }
+         }),
+      ],
    },
    output: {
       path: path.resolve(__dirname, 'wwwroot'),
@@ -13,6 +30,17 @@ module.exports = {
          type: 'module'
       }
    },
+   plugins: [
+      new CompressionPlugin({
+         algorithm: 'gzip',
+         test: /\.(js|css|html|svg)$/
+      }),
+      new CompressionPlugin({
+         algorithm: 'brotliCompress',
+         compressionOptions: { level: 11 },
+         test: /\.(js|css|html|svg)$/
+      })
+   ],
    resolve: {
       fallback: {
          buffer: require.resolve('buffer/'),
