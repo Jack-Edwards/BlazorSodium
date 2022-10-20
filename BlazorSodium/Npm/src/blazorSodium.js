7d061bbaf4fe53378e85285175fb7721feb24f6d
Jack-Edwards <jackedwards@protonmail.com>
2022-10-20 16:23:06 -0500

    Log a single message on sodium init

--- BlazorSodium/Npm/src/blazorSodium.js
+++ BlazorSodium/Npm/src/blazorSodium.js
@@ -2,9 +2,9 @@
 
 export async function init() {
    await sodium.ready;
-   console.log(`Using sodium version: ${sodium.sodium_version_string()}`);
 
    var usingWasm = typeof (sodium.libsodium.asm) === 'object'
       && typeof (sodium.libsodium.asm.__proto__) === 'undefined';
-   console.log(`Sodium is using wasm: ${usingWasm}`);
+
+   console.log(`Sodium init: { version: ${sodium.sodium_version_string()}, wasm: ${usingWasm} }`);
 }
