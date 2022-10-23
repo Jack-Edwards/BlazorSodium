029637806f99e98b3651c6cb6a8c1eaa0c1a1ae2
Jack-Edwards <jackedwards@protonmail.com>
2022-10-22 20:08:17 -0500

    Remove some dead constants

--- BlazorSodium/Sodium/StreamCipher.Constants.cs
+++ BlazorSodium/Sodium/StreamCipher.Constants.cs
@@ -2,16 +2,6 @@
 {
    public static partial class StreamCipher
    {
-      public static uint KEY_BYTES
-      {
-         get => (uint)Sodium.GetConstantNumber("crypto_stream_KEYBYTES");
-      }
-
-      public static uint NONCE_BYTES
-      {
-         get => (uint)Sodium.GetConstantNumber("crypto_stream_NONCEBYTES");
-      }
-
       public static uint CHACHA20_KEY_BYTES
       {
          get => (uint)Sodium.GetConstantNumber("crypto_stream_chacha20_KEYBYTES");
@@ -32,36 +22,6 @@
          get => (uint)Sodium.GetConstantNumber("crypto_stream_chacha20_ietf_NONCEBYTES");
       }
 
-      public static uint SALSA2012_KEY_BYTES
-      {
-         get => (uint)Sodium.GetConstantNumber("crypto_stream_salsa2012_KEYBYTES");
-      }
-
-      public static uint SALSA2012_NONCE_BYTES
-      {
-         get => (uint)Sodium.GetConstantNumber("crypto_stream_salsa2012_NONCEBYTES");
-      }
-
-      public static uint SALSA208_KEY_BYTES
-      {
-         get => (uint)Sodium.GetConstantNumber("crypto_stream_salsa208_KEYBYTES");
-      }
-
-      public static uint SALSA208_NONCE_BYTES
-      {
-         get => (uint)Sodium.GetConstantNumber("crypto_stream_salsa208_NONCEBYTES");
-      }
-
-      public static uint SALSA20_KEY_BYTES
-      {
-         get => (uint)Sodium.GetConstantNumber("crypto_stream_salsa20_KEYBYTES");
-      }
-
-      public static uint SALSA20_NONCE_BYTES
-      {
-         get => (uint)Sodium.GetConstantNumber("crypto_stream_salsa20_NONCEBYTES");
-      }
-
       public static uint XCHACHA20_KEY_BYTES
       {
          get => (uint)Sodium.GetConstantNumber("crypto_stream_xchacha20_KEYBYTES");
@@ -70,16 +30,6 @@
       public static uint XCHACHA20_NONCE_BYTES
       {
          get => (uint)Sodium.GetConstantNumber("crypto_stream_xchacha20_NONCEBYTES");
-      }
-
-      public static uint XSALSA20_KEY_BYTES
-      {
-         get => (uint)Sodium.GetConstantNumber("crypto_stream_xsalsa20_KEYBYTES");
-      }
-
-      public static uint XSALSA20_NONCE_BYTES
-      {
-         get => (uint)Sodium.GetConstantNumber("crypto_stream_xsalsa20_NONCEBYTES");
       }
    }
 }
