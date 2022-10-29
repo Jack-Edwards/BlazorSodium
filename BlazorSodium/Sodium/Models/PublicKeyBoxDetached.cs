592b7ae8fc595ff7f677ba771e1990b79ba970f0
Jack-Edwards <jackedwards@protonmail.com>
2022-10-29 15:26:03 -0500

    Implement methods and constants for PublicKeyBox

---
+++ BlazorSodium/Sodium/Models/PublicKeyBoxDetached.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using System.Runtime.Versioning;
+
+namespace BlazorSodium.Sodium.Models
+{
+   public class PublicKeyBoxDetached
+   {
+      public byte[] MessageAuthenticationCode { get; init; }
+      public byte[] Cipher { get; init; }
+
+      public PublicKeyBoxDetached(byte[] messageAuthenticationCode, byte[] cipher)
+      {
+         MessageAuthenticationCode = messageAuthenticationCode;
+         Cipher = cipher;
+      }
+
+      [SupportedOSPlatform("browser")]
+      public static PublicKeyBoxDetached FromJavaScript(JSObject jsObject)
+      {
+         byte[] mac = jsObject.GetPropertyAsByteArray("mac");
+         byte[] cipher = jsObject.GetPropertyAsByteArray("ciphertext");
+         return new PublicKeyBoxDetached(mac, cipher);
+      }
+   }
+}
---
+++ BlazorSodium/Sodium/SealedPublicKeyBox.cs
@@ -0,0 +1,38 @@
+﻿using BlazorSodium.Sodium.Models;
+using System.Runtime.InteropServices.JavaScript;
+using System.Runtime.Versioning;
+
+namespace BlazorSodium.Sodium
+{
+   public static partial class SealedPublicKeyBox
+   {
+      /// <summary>
+      /// Encrypts a message using only the recipient's public key.
+      /// </summary>
+      /// <param name="message"></param>
+      /// <param name="publicKey"></param>
+      /// <returns></returns>
+      [JSImport("sodium.crypto_box_seal", "blazorSodium")]
+      public static partial byte[] Crypto_Box_Seal(byte[] message, byte[] publicKey);
+
+      /// <summary>
+      /// Encrypts a message using only the recipient's public key.
+      /// </summary>
+      /// <param name="message"></param>
+      /// <param name="publicKey"></param>
+      /// <returns></returns>
+      [JSImport("sodium.crypto_box_seal", "blazorSodium")]
+      public static partial byte[] Crypto_Box_Seal(string message, byte[] publicKey);
+
+      /// <summary>
+      /// Decrypts a ciphertext using the recipient's key pair.
+      /// </summary>
+      /// <param name="ciphertext"></param>
+      /// <param name="keyPair"></param>
+      /// <returns></returns>
+      /// <see cref="https://github.com/jedisct1/libsodium.js/blob/master/wrapper/symbols/crypto_box_seal_open.json"/>
+      [SupportedOSPlatform("browser")]
+      public static byte[] Crypto_Box_Seal_Open(byte[] ciphertext, X25519KeyPair keyPair)
+         => Crypto_Box_Seal_Open_Internal(ciphertext, keyPair.PublicKey, keyPair.PrivateKey);
+   }
+}
