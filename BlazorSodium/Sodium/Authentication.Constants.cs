6a10589d1fe5cc1ff409e0ffccdb3a21fa06e82d
Jack-Edwards <jackedwards@protonmail.com>
2022-10-29 16:24:56 -0500

    Add methods and constants for Authentication

---
+++ BlazorSodium/Sodium/Authentication.Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorSodium.Sodium
+{
+   public static partial class Authentication
+   {
+      public static uint BYTES
+      {
+         get => (uint)Sodium.GetConstantNumber("crypto_auth_BYTES");
+      }
+
+      public static uint KEY_BYTES
+      {
+         get => (uint)Sodium.GetConstantNumber("crypto_auth_KEYBYTES");
+      }
+   }
+}
